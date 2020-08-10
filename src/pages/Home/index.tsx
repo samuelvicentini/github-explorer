cafed3fae37ff5a0f0f6e168062848849d1bd957
Samuel Vicentini <samuelvicentini@outlook.com>
2020-08-10 14:50:23 -0300

    create input on home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import logoImg from '../../assets/logo.svg';
+
 import * as S from './styles';
 
 const Home: React.FC = () => {
-  return <S.Title>Home</S.Title>;
+  return (
+    <>
+      <img src={logoImg} alt="logo" />
+      <S.Title>Explore repositórios no Github.</S.Title>
+      <S.Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </S.Form>
+    </>
+  );
 };
 
 export default Home;
